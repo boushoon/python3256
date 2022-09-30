29a158ecf5c02e2a6c8050198a742737b3933e4b
boushoon <sadfik94@gmail.com>
2022-09-30 13:27:12 +0300

    Добавил библиотеку math

--- task1.py
+++ task1.py
@@ -1,11 +1,15 @@
+from math import factorial
 def main():
     n = int(input())
     res = 1
     if n < 0:
         print("Введите неотрицательное число!")
         return 0
-    for i in range (2, n + 1):
-        res *= i
+    #for i in range (2, n + 1):
+    #    res *= i
+    res = factorial(n)
     print(f"{n}! = {res}")
+
+
 
 main()
